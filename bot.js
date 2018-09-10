44bafef8d10a02b1d0ae7774f95085d82e422bf2
bakutkisa241 <35742714+bakutkisa241@users.noreply.github.com>
2018-09-10 19:24:20 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**السلام عليكم
+سيرفرنا جميل وينقصنا جمالك
+[ __ https://discord.gg/rjuhYFY __]
+حياك ي عيني ...**
+`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**السلام عليكم
+سيرفرنا جميل وينقصنا جمالك
+[ __ https://discord.gg/rjuhYFY __]
+حياك ي عيني ...**`) 
+}).catch(console.error)
+})
+
+
+client.login(process.env.BOT_TOKEN);
